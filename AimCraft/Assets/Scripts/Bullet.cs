1d2def51b865dc29a5f9cfd1608ee99befdee3de
WhiteCl0ud01 <136679162+WhiteCl0ud01@users.noreply.github.com>
2024-06-01 14:53:47 +0800

    Shooting Gun

---
+++ AimCraft/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifeTime = 2f;
+
+    void Start()
+    {
+        // Remove the bullet after 2 seconds
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        // shoot the bullets towards the target
+        transform.Translate(Vector3.forward * speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Destroy the bullet on impact
+        Destroy(gameObject);
+    }
+}
+
---
+++ AimCraft/Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+    public float fireRate = 0.5f;
+    private float nextFireTime;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        // Instantiate the bullet at the spawn point
+        Quaternion bulletRotation = transform.rotation * Quaternion.Euler(0, 0, 90);
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletRotation);
+    }
+}
