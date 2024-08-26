49110fef5dc5027fb90aa2acee88e018404bc959
WhiteCl0ud01 <rmanbayjm@gmail.com>
2024-08-27 01:20:26 +0800

    Adding suggestions

--- AimCraft/Assets/Scripts/GamemodeNStart.cs
+++ AimCraft/Assets/Scripts/GamemodeNStart.cs
@@ -22,6 +22,16 @@ public class GamemodeNStart : MonoBehaviour
     public TextMeshProUGUI hitRateText; //Hit Rate text
     public TextMeshPro startStopText; //Start and Stop text
     public TextMeshPro gamemodeText;
+    // Options
+    public TextMeshPro precisionText;
+    public TextMeshPro flickText;
+    public TextMeshPro survivalText;
+    public TextMeshPro Timer1Text;
+    public TextMeshPro Timer2Text;
+    public TextMeshPro Timer3Text;
+    public TextMeshPro Timer4Text;
+    //Tutorial
+    public TextMeshPro gamemodeTutorialText;
     public string gamemodeType;
     public string prevgamemodeType;
     private float timePassed;
@@ -37,7 +47,7 @@ public class GamemodeNStart : MonoBehaviour
     void Start()
     {
         nextSpawnTime = Time.time + spawnInterval;
-        gamemodeType = "Flick";
+        gamemodeType = "Flick Test";
         Obstacles.SetActive(false);
         audioSource = audioSourceOrigin.GetComponent<AudioSource>();
     }
@@ -49,7 +59,7 @@ public class GamemodeNStart : MonoBehaviour
             timer -= Time.deltaTime; // Count down to 0
             // Update the timer text on the screen
             UpdateTimerText();
-            if(gamemodeType == "Flick" || gamemodeType == "Precision")
+            if(gamemodeType == "Flick Test" || gamemodeType == "Precision")
             {
                 if(Time.time >= nextSpawnTime)
                 {
@@ -236,23 +246,57 @@ public class GamemodeNStart : MonoBehaviour
             if (timing == "15Second")
             {
                 setTime = 15f;
+                Timer1Text.color = Color.blue;
+                Timer2Text.color = Color.white;
+                Timer3Text.color = Color.white;
+                Timer4Text.color = Color.white;
                 timeSelectedText.text = "Time Selected: 15 sec";
             }
             else if (timing == "30Second")
             {
+                Timer1Text.color = Color.white;
+                Timer2Text.color = Color.blue;
+                Timer3Text.color = Color.white;
+                Timer4Text.color = Color.white;
                 setTime = 30f;
                 timeSelectedText.text = "Time Selected: 30 sec";
             }
             else if (timing == "45Second")
             {
+                Timer1Text.color = Color.white;
+                Timer2Text.color = Color.white;
+                Timer3Text.color = Color.blue;
+                Timer4Text.color = Color.white;
                 setTime = 45f;
                 timeSelectedText.text = "Time Selected: 45 sec";
             }
             else if (timing == "60Second")
             {
+                Timer1Text.color = Color.white;
+                Timer2Text.color = Color.white;
+                Timer3Text.color = Color.white;
+                Timer4Text.color = Color.blue;
                 setTime = 60f;
                 timeSelectedText.text = "Time Selected: 60 sec";
             }
+        }
+    }
+    public void displayTimerText(bool set)
+    {
+        if(set)
+        {
+            Timer1Text.text = "15 sec";
+            Timer2Text.text = "30 sec";
+            Timer3Text.text = "45 sec";
+            Timer4Text.text = "60 sec";
+        }
+        else
+        {
+            Timer1Text.text = "";
+            Timer2Text.text = "";
+            Timer3Text.text = "";
+            Timer4Text.text = "";
+
         }
     }
     public void gamemodeSelector(string gamemode)
@@ -261,8 +305,13 @@ public class GamemodeNStart : MonoBehaviour
         UpdateGameModeText();
         if (gamemode == "Survival")
         {
+            precisionText.color = Color.white;
+            survivalText.color = Color.blue;
+            flickText.color = Color.white;
+            displayTimerText(false);
             Obstacles.SetActive(false);
-            UpdateGameModeText();
+
+            gamemodeTutorialText.text = "In Survival mode, 2 targets are always visible within the range. Players have 30 seconds to shoot as many targets as possible, with each kill adding 0.1 seconds to the timer for extended playtime.";
             changeTimer("");
             UpdateMissedText();
             UpdateHitRateText();
@@ -271,10 +320,15 @@ public class GamemodeNStart : MonoBehaviour
             zPosRange[0] = -6f;
             zPosRange[1] = 9f;
             setTime = 30f;
-
+            
         }
         else if (gamemode == "Precision")
         {
+            precisionText.color = Color.blue;
+            survivalText.color = Color.white;
+            flickText.color = Color.white;
+            displayTimerText(true);
+            gamemodeTutorialText.text = "In Precision mode, obstacles are placed to partially obscure the targets, so only a portion of each target is visible. Targets only spawn at the back of the range.";
             Obstacles.SetActive(true);
             changeTimer(setTime.ToString() + "Second");
             UpdateMissedText();
@@ -284,8 +338,13 @@ public class GamemodeNStart : MonoBehaviour
             zPosRange[0] = 8f;
             zPosRange[1] = 9f;
         }
-        else if (gamemode == "Flick")
+        else if (gamemode == "Flick Test")
         {
+            precisionText.color = Color.white;
+            survivalText.color = Color.white;
+            flickText.color = Color.blue;
+            displayTimerText(true);
+            gamemodeTutorialText.text = "In Flick Test mode, targets appear and disappear within 0.5 seconds and spawn randomly across the entire available range.";
             Obstacles.SetActive(false);
             changeTimer(setTime.ToString() + "Second");
             UpdateMissedText();
@@ -294,9 +353,6 @@ public class GamemodeNStart : MonoBehaviour
             xPosRange[1] = 4f;
             zPosRange[0] = -6f;
             zPosRange[1] = 9f;
-
-
         }
-
     }
 }
--- AimCraft/Assets/Scripts/TargetDemo.cs
+++ AimCraft/Assets/Scripts/TargetDemo.cs
@@ -6,19 +6,42 @@ using TMPro;
 public class TargetDemo : MonoBehaviour
 {
     public TextMeshPro demoText;
+    public int shotHit;
+    private string[] messages = {
+          "Don't shoot me!",
+          "Please start the game!",
+          "You got this!",
+          "Take your best shot!",
+          "Aim carefully!",
+          "Stay focused!",
+          "Nice move!",
+          "Stay sharp!",
+          "Stay calm!"
+     };
     void Start()
     {
+        
     }
 
     public void OnHit(string bodyparts)
     {
-        if (bodyparts == "BodyHitbox")
+        shotHit++;
+        float randomValue = Random.Range(0f, 1f);
+        if (shotHit > 10 && randomValue > 0.7)
         {
-            demoText.text = "Body shot deals 40 damage which needs 4 shots to kill the target";
+            int randomInt = Random.Range(0, 8);
+            demoText.text = messages[randomInt];
         }
-        else if (bodyparts == "HeadHitbox")
+        else
         {
-            demoText.text = "Headshot deals 150 damage which kills the target instantly";
+            if (bodyparts == "BodyHitbox")
+            {
+                demoText.text = "Body shot deals 40 damage which needs 4 shots to kill the target";
+            }
+            else if (bodyparts == "HeadHitbox")
+            {
+                demoText.text = "Headshot deals 150 damage which kills the target instantly";
+            }
         }
     }
 }
